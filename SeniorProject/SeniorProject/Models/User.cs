d7d77f13755d3e78126c0172e0cb560f9d0e3837
berkayyceylann <se.berkayceylan@gmail.com>
2024-01-09 18:11:54 +0300

    adding setup and requirements
    - create models
    - create dbcontext
    -create account and register controller
    - create register and login functions

---
+++ SeniorProject/SeniorProject/Models/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SeniorProject.Models;
+
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid UserId { get; set; }
+
+    [Required]
+    [MaxLength(256)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(256)]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(256)]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    // Navigation property - Bir kullanıcının birden fazla pet'i olabilir
+    public virtual ICollection<Pet> Pets { get; set; }
+}
